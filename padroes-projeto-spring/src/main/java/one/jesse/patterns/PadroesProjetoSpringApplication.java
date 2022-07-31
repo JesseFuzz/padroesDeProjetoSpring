3bf7ff28b8559147f121902cfb63327f7fbb197f
Jesse <jessesouzaall@gmail.com>
2022-07-31 17:40:14 -0300

    finalizado mas com alguns detalhes a serem considerados

--- padroes-projeto-spring/src/main/java/one/jesse/patterns/PadroesProjetoSpringApplication.java
+++ padroes-projeto-spring/src/main/java/one/jesse/patterns/PadroesProjetoSpringApplication.java
@@ -2,21 +2,13 @@ package one.jesse.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@RestController
+@EnableFeignClients
 public class PadroesProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PadroesProjetoSpringApplication.class, args);
 	}
-	
-	@GetMapping("/teste")
-	public String teste() {
-		return "Olá, mundo";
-	}
-
 }
